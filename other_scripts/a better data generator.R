c3d4211202a14f96dd83e26413869405b77118b4
bdrayton <bradleydrayton@gmail.com>
2022-06-02 13:48:12 +1200

    nearly finished a new data generator

--- other_scripts/a better data generator.R
+++ other_scripts/a better data generator.R
@@ -52,82 +52,95 @@ one_dataset
 #'
 #'
 
-one_dataset <- function(formula, dists, dist_args){
+get_term.labels <- function(formula_terms){
 
-  # decompose formula and check against names in dists.
-
-
-  vars <- lapply(dists, lazyeval::f_eval, data = dist_args)
-
-  data.frame(vars)
+  attr(formula_terms, "term.labels")
 
 }
 
-ds <- one_dataset(dists = list(X1 = ~rnorm(n, 0, 1),
+get_group_label <- function(random_term){
+
+  term_list <- coxme:::formula2(random_term)
+
+  as.character(term_list$group)
+
+}
+
+check_var_labels <- function(var_labels, dist_labels){
+
+  test <- var_labels %in% dist_labels
+
+  not_in_dist_labels <- paste0(var_labels[!test], collapse = ", ")
+
+  if(!all(test))
+    stop(paste0("All variables in formula must have be present in dists.\n  These variables are in your formula but not in dists: ", not_in_dist_labels))
+
+}
+
+
+one_dataset <- function(formula, dists, dist_args, coefficients, random_effects_variance, seed = NULL){
+
+  if(!is.null(seed)) set.seed(seed)
+
+  flist <- coxme:::formula1(formula)
+
+  fixed_terms <- stats::terms(flist$fixed)
+
+  fixed_term_labels <- get_term.labels(fixed_terms)
+
+  random_term_labels <- sapply(flist$random, get_group_label)
+  names(random_term_labels) <- random_term_labels
+
+  term_labels <- c(fixed_term_labels, random_term_labels)
+
+  check_var_labels(term_labels, names(dists))
+
+  vars <- lapply(dists[term_labels], lazyeval::f_eval, data = dist_args)
+
+  vars_df <- data.frame(vars)
+
+  lp_random_effects <- sapply(random_term_labels, function(re){
+
+    re_data <- vars_df[, re, drop = TRUE]
+
+    re_factor <- factor(re_data, levels = unique(re_data))
+
+    group_counts <- table(re_factor)
+
+    b <- rnorm(length(group_counts), mean = 0, sd = sqrt(random_effects_variance[[re]]))
+
+    b_rep <- dplyr::left_join(data.frame(re_name = re_factor),
+                              data.frame(re_name = names(group_counts), re = b), by = "re_name")
+
+    b_rep$re
+
+  }, simplify = "matrix")
+
+  risk_score <- .rowSums(cbind(as.matrix(vars_df[, fixed_term_labels]) %*% coefficients, lp_random_effects),
+                         m = nrow(lp_random_effects), n = ncol(lp_random_effects) + 1)
+
+  error <- lazyeval::f_eval(dists$error, data = dist_args)
+
+  vars_df$stat_time <- exp(-risk_score) * error
+
+  vars_df$stat <- lazyeval::f_eval(dists$error, data = dist_args)
+
+  vars_df
+
+}
+
+debugonce(one_dataset)
+
+ds <- one_dataset(formula = survival::Surv(stat_time, time) ~ X1 + X2 + X3 + (1 | M1),
+                  dists = list(X1 = ~rnorm(n, 0, 1),
                                X2 = ~rep(rnorm(k, 0, 1), each = nk),
                                X3 = ~rep(rep(c(1, 0), ceiling(k/2))[seq_len(k)], each = nk),
                                M1 = ~rep(1:k, each = nk),
-                               M2 = ~rep(c("l", "r"), ceiling(n/2))[seq_len(n)]),
-                  dist_args = list(k = 10, nk = 10, n = 100))
-
-ds
-
-test_formula <- formula("Y ~ X1 + X2 + X3 + (1|M/M2)")
-
-lme4:::subbars(test_formula)
-
-lme4:::getFixedFormula(test_formula)
-
-lme4:::findbars
-
-coxme:::expand.nested(coxme:::formula1(test_formula)$random[[1]])
-
-coxme:::findIntercept
-
-coxme:::formula1(test_formula)
-
-coxme:::formula2(test_formula)
-
-
-lme4:::getCovariateFormula(test_formula)
-
-
-fr.form <- lme4:::subbars(test_formula)
-environment(fr.form) <- environment(test_formula)
-
-mf$formula <- fr.form
-
-lme4::lFormula(test_formula)
-
-fr <- eval(mf, parent.frame())
-
-fr <- factorize(fr.form, fr, char.only = TRUE)
-attr(fr, "formula") <- formula
-
-n <- nrow(fr)
-reTrms <- mkReTrms(lme4:::findbars(lme4:::RHSForm(test_formula)), fr)
-
-lme4::mkReTrms(lme4:::findbars(lme4:::RHSForm(test_formula)), fr)
-
-findbars(test_formula)
-
-lme4:::reOnly(test_formula)
-
-
-my_fun <- function(k, stuff_using_k){
-
-  lazyeval::f_eval(stuff_using_k, data = list(k = k))
-
-}
-
-my_fun(k = 10, stuff_using_k = ~rnorm(n = k))
-
-
-f <- ~rnorm(n = k)
-
-lazyeval::f_eval(f)
-
-
-
+                               M2 = ~rep(c("l", "r"), ceiling(n/2))[seq_len(n)],
+                               error = ~rexp(n, 10),
+                               stat = ~sample(rep(c(0, 1), round(n * c(0.2, 0.8))), n)),
+                  dist_args = list(k = 50, nk = 4, n = 50*4),
+                  random_effects_variance = list(M1 = 1, M2 = 2),
+                  coefficients = c(1, -0.7, 0.5))
 
 
